13553e9e124166729d5d63b0996e0cbd5d2cc278
eberdelgado <eberdelgado2014@outlook.com>
2023-10-25 09:57:31 -0300

    Enum OrderStatus

--- src/main/java/com/sll/course/config/TestConfig.java
+++ src/main/java/com/sll/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.sll.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.sll.course.entities.Order;
 import com.sll.course.entities.User;
+import com.sll.course.entities.enums.OrderStatus;
+import com.sll.course.repositories.OrderRepository;
 import com.sll.course.repositories.UserRepository;
 
 @Configuration
@@ -15,14 +19,21 @@ import com.sll.course.repositories.UserRepository;
 public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
-
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u1); 
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 	
 	
---
+++ src/main/java/com/sll/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.sll.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sll.course.entities.Order;
+import com.sll.course.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
