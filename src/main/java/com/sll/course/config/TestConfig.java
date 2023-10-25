8439501b56707456a6f1e2f9d402c9fbda31a580
eberdelgado <eberdelgado2014@outlook.com>
2023-10-25 10:52:46 -0300

    Entity category

--- src/main/java/com/sll/course/config/TestConfig.java
+++ src/main/java/com/sll/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.sll.course.entities.Category;
 import com.sll.course.entities.Order;
 import com.sll.course.entities.User;
 import com.sll.course.entities.enums.OrderStatus;
+import com.sll.course.repositories.CategoryRepository;
 import com.sll.course.repositories.OrderRepository;
 import com.sll.course.repositories.UserRepository;
 
@@ -21,10 +23,16 @@ public class TestConfig implements CommandLineRunner{
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -34,6 +42,7 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 	
 	
---
+++ src/main/java/com/sll/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.sll.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sll.course.entities.Category;
+import com.sll.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
